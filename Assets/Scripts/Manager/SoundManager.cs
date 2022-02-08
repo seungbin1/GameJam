d27add550c4ba8854261ca192709337b9c7f5fcc
seungbin1 <71053397+seungbin1@users.noreply.github.com>
2022-02-08 14:40:34 +0900

    사운드, UI JSon

--- Assets/Scripts/Manager/SoundManager.cs
+++ Assets/Scripts/Manager/SoundManager.cs
@@ -4,5 +4,53 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    public static SoundManager instance;
+
+    private AudioSource button, game, jump;
+
+    enum Audio
+    {
+        button, 
+        game, 
+        jump
+    }
+
+    private void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        button = transform.Find("Button").GetComponent<AudioSource>();
+        game = transform.Find("Game").GetComponent<AudioSource>();
+        jump = transform.Find("Jump").GetComponent<AudioSource>();
+    }
+
+    private void OnDestroy()
+    {
+        if(instance == this)
+        {
+            instance = null;
+        }
+    }
+
+
+    public AudioSource GetButton()
+    {
+        return button;
+    }    
+    
+    public AudioSource GetGame()
+    {
+        return game;
+    }   
+    
+    public AudioSource GetJump()
+    {
+        return jump;
+    }
+
 
 }
